8e1a58e6ac3905f463195e90d173a750e8dea349
Daniel Brož <daniel.broz@o2.cz>
2023-06-19 06:03:33 +0200

    Add animation to menu buttons

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,20 +1,21 @@
 const Menu = () => {
+    console.log('rendering menu');
     return (
     <nav className={`desktop:block laptop:block hidden h-full`}>
         <ul className="desktop:flex laptop:flex h-full hidden">
-          <a href="#" className={`menu-button bounce-to_bottom`}>
+          <a href="#" className={`menu-button bounce-to-bottom`}>
               Domů
           </a>
-          <a href="#" className={`menu-button bounce-to_bottom`} >
+          <a href="#" className={`menu-button bounce-to-bottom`} >
               O nás
           </a>
-          <a href="#" className={`menu-button bounce-to_bottom`}>
+          <a href="#" className={`menu-button bounce-to-bottom`}>
               Naše služby
           </a>
-          <a href="#" className={`menu-button bounce-to_bottom`}>
+          <a href="#" className={`menu-button bounce-to-bottom`}>
               Reference
           </a>
-          <a href="#" className={`menu-button bounce-to_bottom`}>
+          <a href="#" className={`menu-button bounce-to-bottom`}>
               Kontakt
           </a>
         </ul>
