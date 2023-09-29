f32e3d317387225314cdbb6a608df9f949e911a9
Daniel Brož <daniel.broz@o2.cz>
2023-09-29 20:10:12 +0200

    bugfix

--- src/components/ui/AboutCard.tsx
+++ src/components/ui/AboutCard.tsx
@@ -17,7 +17,7 @@ const AboutCard: React.FC<Props> = ({
 		<div
 			className={`${
 				className ||
-				'flex flex-col items-center justify-start w-full h-full bg-white rounded-xl shadow-xl'
+				'flex flex-col items-center justify-start w-full h-auto bg-white rounded-xl shadow-xl'
 			}`}
 		>
 			<div className="w-full h-3/5 p-8 flex justify-center items-center rounded-t-xl max-h-72 mb-4">
@@ -30,7 +30,7 @@ const AboutCard: React.FC<Props> = ({
 				/>
 			</div>
 
-			<div className="flex flex-col justify-center items-center w-full h-2/5 p-4">
+			<div className="flex flex-col justify-center items-center w-full  p-4">
 				<h3 className="text-2xl font-bold text-center">{title}</h3>
 				<p className="text-lg text-center">{description}</p>
 			</div>
