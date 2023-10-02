3439afc11809d713e4da22a9062f89b9ad3fcfd8
Daniel Brož <daniel.broz@o2.cz>
2023-10-02 11:27:17 +0200

    Add Google Analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import PageLayout from '@dopos/components/pages/PageLayout';
 import { PageContextProvider } from '@dopos/contexts/PageContext';
+import Script from 'next/script';
 
 const Home = async () => {
 	return (
 		<PageContextProvider>
+			<Script async src="https://www.googletagmanager.com/gtag/js?id=G-HC65LENXQR"></Script> 
+			<Script id="google-analytics"> 
+				{`
+					window.dataLayer = window.dataLayer || [];   
+					function gtag(){dataLayer.push(arguments);}   
+					gtag('js', new Date());
+				
+					gtag('config', 'G-HC65LENXQR');
+				`} 
+			</Script>
 			<PageLayout />
 		</PageContextProvider>
 	);
